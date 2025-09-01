5c1701fec88ff63873b35e7f0bbca7b72318308a
Vansh <vanshgupta987654321@gmail.com>
2025-09-01 21:51:35 +0530

    landing page with animation and input field

---
+++ client/src/socket.js
@@ -0,0 +1,18 @@
+import { io } from "socket.io-client";
+
+const socket=io(`http://localhost:4000`);//frontend socket connection with backend server.io
+
+export const joinRoom=(roomId)=>{//Frontend se backend ko event bhej rahe ho (joinRoom), taki user ek particular room me join ho jaye.
+    socket.emit("joinRoom",roomId);
+}
+
+export const emitLocationUpdate=(data)=>{//Frontend apna location data backend ko bhejta hai.
+    socket.emit("locationUpdate",data);
+}
+
+export const listenforUsersUpdates=(callback)=>{//Frontend backend ke events sunega (jaise koi user offline hua).
+    socket.on('user-offline',(callback)=>{
+    })
+}
+
+export default socket;
